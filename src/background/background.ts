6f4e0d5a71dc10ea924a0163a5a84b6af57b8b3b
Hanzla Sibghat <hanzlasib@gmail.com>
2025-02-07 09:52:38 +0100

    blocka ll images on newyork time pages

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,8 +1,23 @@
 chrome.webRequest.onBeforeRequest.addListener(
   (details) => {
     console.log(details);
+    const url = details.url;
+    //all images contain the static work in url thats why all images are block on newyork time website
+    const filters = [
+      "googleadsservices",
+      "googlesyndication",
+      "g.doubleclick",
+      "static01",
+    ];
+    for (const filter of filters) {
+      if (url.indexOf(filter) != -1) {
+        return {
+          cancel: true,
+        };
+      }
+    }
     return {
-      // cancel: true,
+      cancel: false,
     };
   },
   {
